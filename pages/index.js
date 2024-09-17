da4057c46d52681306e8edbb5310f698c1ab5b97
berabhishek <bera.abhishek21@gmail.com>
2024-09-17 23:17:45 +0530

    added to index

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import localFont from "next/font/local";
-
+import TopPhones from "./top-phones";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -14,9 +14,8 @@ const geistMono = localFont({
 
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
+    <div>
+      <TopPhones />
     </div>
   );
 }
